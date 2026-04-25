108b188c195e85db5af167776fbe20d6ec5146b1
lucky2007pro <usa20070302@gmail.com>
2026-04-25 12:57:05 +0500

    Fix session declaration

--- controllers/AuthController.cc
+++ controllers/AuthController.cc
@@ -57,6 +57,7 @@ void AuthController::handleLogin(const HttpRequestPtr& req, std::function<void(c
         [callback, req](const drogon::orm::Result& result) {
             if (result.size() > 0) {
                 // Foydalanuvchi topildi! Session'ga saqlaymiz
+                auto session = req->session();
                 std::string role = result[0]["role"].as<std::string>();
                 session->insert("user_id", result[0]["id"].as<int>());
                 session->insert("user_name", result[0]["full_name"].as<std::string>());
